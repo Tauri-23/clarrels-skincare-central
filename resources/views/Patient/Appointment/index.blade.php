c189e9352e739c21976b14b069f4ca0d9606cd7b
Tauri-23 <airichjaydiawan@gmail.com>
2024-05-05 17:10:03 +0800

    hahahah
    hahaha

--- resources/views/Patient/Appointment/index.blade.php
+++ resources/views/Patient/Appointment/index.blade.php
@@ -50,7 +50,7 @@
                             <div class="txt-l2">{{$appointment->doctors()->first()->firstname}} {{$appointment->doctors()->first()->lastname}}</div>
                             <div class="txt-m3">{{$appointment->doctors()->first()->doctor_type}}</div>
                         </div>
-                        <div class="justify-self-end">Appointment Date: {{ \Carbon\Carbon::parse($appointment->appointment_date)->format('D M d, Y') }}</div>
+                        <div class="justify-self-end">Appointment Date: {{ \Carbon\Carbon::parse($appointment->appointment_date)->format('D M d, Y') }} {{ \Carbon\Carbon::parse($appointment->appointment_time)->format('g:i a') }}</div>
                     </div>
                 </div>
             @endforeach
--- resources/views/components/modals.blade.php
+++ resources/views/components/modals.blade.php
@@ -25,4 +25,20 @@
             </div>
         </div>
     </div>
+
+
+
+
+@elseif($modalType == "info-yn")
+    <div class="modal1 d-none info-yn-modal" id="info-yn-modal">
+        <div class="modal1-box-small modal-text">
+            <i id="modal-close-btn" class="modal1-x-icon fa-solid fa-xmark"></i>
+            <div class="modal1-txt text-center">
+            </div>
+            <div class="d-flex justify-content-center gap1 mar-top-2">
+                <a class="primary-btn2-small" id="modal-close-btn">No</a>
+                <a class="yes-btn primary-btn3-small">Yes</a>
+            </div>
+        </div>
+    </div>
 @endif
