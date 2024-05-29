fd36cebb58d8f66fc2adf25e09cd2c2365e74711
Tauri-23 <airichjaydiawan@gmail.com>
2024-05-29 14:34:36 +0800

    asd
    asd

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,32 +1,63 @@
-<div class="footer d-flex">
-    {{-- Content1 --}}
-    <div class="d-flex flex-direction-y gap1">
-        <div class="top-nav-logo">
-            <img src="/assets/media/logos/logo2.jpeg" alt="">
+<div class="footer">
+    
+    <div class="d-flex justify-content-between">
+        {{-- Content1 --}}
+        <div class="d-flex flex-direction-y gap1">
+            <div class="top-nav-logo">
+                <img src="/assets/media/logos/logo2.jpeg" alt="">
+            </div>
+
+            <div class="txt-l3">
+                Risus commodo viverra maecenas accumsan <br>
+                lacus vel facilisis volutpat est Elit <br>
+                pellentesque habitant.
+            </div>
+
+            <div class="d-flex flex-direction-y gap3">
+                <div class="txt-l3">OPEN HOURS</div>
+                <div class="txt-m1">Monday - Saturday : 10 AM - 07 PM</div>
+            </div>
+
+            <div class="d-flex gap2 txt-xl3 color-violet1">
+                <i class="fa-brands fa-square-facebook"></i>
+                <i class="fa-brands fa-square-x-twitter"></i>
+                <i class="fa-brands fa-square-instagram"></i>
+                <i class="fa-brands fa-square-whatsapp"></i>
+            </div>
         </div>
 
-        <div class="txt-l3">
-            Risus commodo viverra maecenas accumsan <br>
-            lacus vel facilisis volutpat est Elit <br>
-            pellentesque habitant.
+        {{-- content2 --}}
+        <div class="d-flex flex-direction-y gap2">
+            <div class="txt-l3 fw-bold">PAGES</div>
+            <div class="d-flex flex-direction-y gap3">
+                <a href="/" class="txt-m1 text-decoration-none color-black2">Home</a>
+                <a href="" class="txt-m1 text-decoration-none color-black2">FAQ's</a>
+                <a href="" class="txt-m1 text-decoration-none color-black2">Services</a>
+                <a href="" class="txt-m1 text-decoration-none color-black2">Contact</a>
+            </div>
         </div>
 
-        <div class="d-flex flex-direction-y gap3">
-            <div class="txt-l3">OPEN HOURS</div>
-            <div class="txt-m1">Monday - Saturday : 10 AM - 07 PM</div>
+        {{-- content3 --}}
+        <div class="d-flex flex-direction-y gap2">
+            <div class="txt-l3 fw-bold">GET HANDLING</div>
+            <div class="d-flex flex-direction-y gap3">
+                <a href="/signinPatient" class="txt-m1 text-decoration-none color-black2">Consultation</a>
+                <a href="/signinPatient" class="txt-m1 text-decoration-none color-black2">Treatments</a>
+            </div>
         </div>
 
-        <div class="d-flex gap2 txt-xl3 color-violet1">
-            <i class="fa-brands fa-square-facebook"></i>
-            <i class="fa-brands fa-square-x-twitter"></i>
-            <i class="fa-brands fa-square-instagram"></i>
-            <i class="fa-brands fa-square-whatsapp"></i>
+        {{-- content4 --}}
+        <div class="d-flex flex-direction-y gap2">
+            <div class="txt-l3 fw-bold">GET IN TOUCH</div>
+            <div class="d-flex flex-direction-y gap3">
+                <div class="txt-m1 color-black2">(+1)422-200-1077</div>
+                <div class="txt-m1 color-black2">company@mail.com</div>
+                <div class="txt-m1 color-black2">1234 Mabuhay St. Brgy. UMAK Taguig City</div>
+            </div>
         </div>
     </div>
 
-    {{-- content2 --}}
-    <div class="d-flex flex-direction-y gap1">
-        
-    </div>
+
+    <div class="text-center txt-l3" style="margin-top: 50px;">© Copyright by Otlum - All right reserved.</div>
 
 </div>
